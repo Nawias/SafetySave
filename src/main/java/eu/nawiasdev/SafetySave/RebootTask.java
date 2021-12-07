555323e11101a8c345bfcd0e056116ed0c423fca
Michał Wójcik <mjtwojcik@wp.pl>
2021-12-07 01:30:26 +0100

    hotfix 2

--- src/main/java/eu/nawiasdev/SafetySave/RebootTask.java
+++ src/main/java/eu/nawiasdev/SafetySave/RebootTask.java
@@ -1,5 +1,7 @@
 package eu.nawiasdev.SafetySave;
 
+import org.bukkit.Bukkit;
+
 import java.util.Locale;
 import java.util.TimerTask;
 
@@ -9,6 +11,8 @@ public class RebootTask extends TimerTask {
     @Override
     public void run() {
         ProcessBuilder builder = new ProcessBuilder();
+        Bukkit.getServer().savePlayers();
+        Bukkit.getServer().getWorlds().get(0).save();
         if(isWindows) {
             builder.command("cmd.exe","shutdown /r -t 0");
         } else {
