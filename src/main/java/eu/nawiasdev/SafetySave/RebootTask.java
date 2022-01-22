c61f5078b98a97664aa6a663b0122bdde526e84f
Michał Wójcik <mjtwojcik@wp.pl>
2022-01-22 01:41:19 +0100

    Properly closing server before reboot

--- src/main/java/eu/nawiasdev/SafetySave/RebootTask.java
+++ src/main/java/eu/nawiasdev/SafetySave/RebootTask.java
@@ -10,24 +10,31 @@ import java.util.TimerTask;
 import java.util.concurrent.Executors;
 import java.util.logging.Logger;
 
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+
 public class RebootTask extends TimerTask {
     private final boolean isWindows = System.getProperty("os.name").toLowerCase(Locale.ROOT).startsWith("windows");
 
     @Override
     public void run() {
+        Bukkit.getServer().savePlayers();
+        for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+            player.kickPlayer("Server is rebooting");
+        }
+        Bukkit.getServer().getWorlds().get(0).save();
         rebootWithRuntime();
+        ConsoleCommandSender sender = new ConsoleCommandSender(Bukkit.getServer());
+        Bukkit.getServer().dispatchCommand(sender,"stop");
     }
 
     private void rebootWithRuntime() {
         String shutdownCommand;
-        Logger logger = Bukkit.getServer().getLogger();
         Runtime runtime = Runtime.getRuntime();
         if(isWindows) {
-            shutdownCommand = "shutdown.exe /r /t 0";
-            logger.info("Rebooting Windows");
+            shutdownCommand = "shutdown.exe /r /t 60";
         } else {
-            logger.info("Rebooting Linux");
-            shutdownCommand = "reboot";
+            shutdownCommand = "shutdown -r +1";
         }
         try {
             runtime.exec(shutdownCommand);
